9b618556385085c3433f58eb62033d0bb76735ed
HoangNguyen679 <ndhoang96@gmail.com>
2022-08-22 22:36:47 +0900

    Add post creator service

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
   def create
+    is_posted = Post::Creator.call(
+      current_user,
+      permitted_params,
+    )
+
+    flash[:alert] = "Something went wrong" unless is_posted
+
+    redirect_back fallback_location: timelines_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:post).permit(
+      :postable_type,
+      :status_text,
+    )
   end
 end
